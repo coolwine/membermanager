659c81b9923fde3087b7497eea69bcf612e0e6a7
Donguk Kim <du@wemakeprice.com>
2014-02-04 18:08:05 +0900

    release_commit2

--- app/controllers/MembermanagerController.php
+++ app/controllers/MembermanagerController.php
@@ -8,6 +8,22 @@ class MembermanagerController extends \Phalcon\Mvc\Controller
 
 		
     }
-
+	
+    
+    public function mainAction()
+    {
+    	$search_val = $this->request->getPost('search_val');
+    	
+    	echo $search_val;
+    	
+    	
+    	log_message('aaaaaaaa');
+    	
+    	
+    	
+    	
+    	$data['search_val'] = $search_val;
+    	$this->view->setVar('data', $data);
+    }
 }
 
